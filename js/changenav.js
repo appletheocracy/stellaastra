1dc149be92fca0406800da4275b65a0688ab8aea
Appo <36673942+appletheocracy@users.noreply.github.com>
2025-05-16 07:42:32 -0400

    Update changenav.js

--- js/changenav.js
+++ js/changenav.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     });
 
     $('.pagination a.nav[href="javascript:Pagination();"]').addClass(stPageNumber);
-    $(".pagination .pag-img").html(function(i, h){
-        return h.replace(/<img src="https://2img.net/i/fa/modernbb/arrow_right.png" alt="Suivant" loading="lazy">/g,' > ');
+    $(".pagination .pag-img").html(function(i, t){
+        return t.replace('<img src="https://2img.net/i/fa/modernbb/arrow_right.png" alt="Suivant" loading="lazy">',' > ');
     });
 });
