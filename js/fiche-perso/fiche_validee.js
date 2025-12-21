b7b316343320bd4770bd3d858c1c57b1525fc372
Appo <36673942+appletheocracy@users.noreply.github.com>
2025-12-21 16:07:43 -0500

    Create fiche_validee.js

---
+++ js/fiche-perso/fiche_validee.js
@@ -0,0 +1,17 @@
+$(function () {
+  var href = window.location.href;
+
+  var map = {
+    '/f8-vampires':  'f8',
+    '/f15-chimeres':'f15',
+    '/f10-hybrides':'f10',
+    '/f19-humains': 'f9'
+  };
+
+  $.each(map, function (path, cls) {
+    if (href.indexOf(path) !== -1) {
+      $('.mainBGN').addClass(cls);
+      return false; // stop dès qu'on trouve
+    }
+  });
+});
